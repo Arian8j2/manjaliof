3bc86e8db818d9c603cb89dc876ad2b0fd07e35f
Arian Rezazadeh <notarian.rez@gmail.com>
2022-10-16 11:11:20 +0330

    add health check

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod input;
 mod db;
 
-use std::process::ExitCode;
+use std::{process::ExitCode, path::Path};
 use db::{Database, jsondb::JsonDb};
 use clap::{Parser, Subcommand};
 use dialoguer::console::style;
@@ -25,18 +25,26 @@ enum Commands {
     Delete,
 
     #[command(about="show all clients")]
-    List
+    List,
+
+    #[command(about="check db and post scripts exist")]
+    HealthCheck
 }
+
+const DB_FILE_PATH: &'static str = "manjaliof-data/data.json";
+const POST_SCRIPTS_FOLDER_PATH: &'static str = "manjaliof-data/post_scripts";
+const POST_SCRIPTS_FILE_NAMES: [&'static str; 3] = ["postadd", "postrenew", "postdelete"];
 
 fn main() -> ExitCode {
     let args = Cli::parse();
-    let db = JsonDb::new("data.json");
+    let db = JsonDb::new(DB_FILE_PATH);
 
     let command_function = match args.command {
         Commands::Add => add_client,
         Commands::Renew => renew_client,
         Commands::Delete => delete_client,
-        Commands::List => todo!()
+        Commands::List => todo!(),
+        Commands::HealthCheck => health_check
     };
 
     if let Err(error) = command_function(&db) {
@@ -67,4 +75,31 @@ fn renew_client(db: &dyn Database) -> Result<(), String> {
 fn delete_client(db: &dyn Database) -> Result<(), String> {
     let name = input::get_client_name();
     db.delete_client(name)
+}
+
+fn health_check(db: &dyn Database) -> Result<(), String> {
+    let db_file = Path::new(DB_FILE_PATH);
+    if !db_file.is_file() {
+        return Err(format!("Cannot find database file at '{}'", DB_FILE_PATH));
+    }
+
+    if db.list_clients().is_err() {
+        return Err("Cannot extract data from db".to_string());
+    }
+
+    let post_scripts_folder = Path::new(POST_SCRIPTS_FOLDER_PATH);
+    if !post_scripts_folder.is_dir() {
+        return Err(format!("Cannot find post scripts folder at '{}'", POST_SCRIPTS_FOLDER_PATH));
+    }
+
+    for post_script_file_name in POST_SCRIPTS_FILE_NAMES {
+        let post_script = post_scripts_folder.join(post_script_file_name);
+        if !post_script.is_file() {
+            return Err(format!("Cannot find post script at '{}'", post_script.to_str().unwrap()));
+        }
+    }
+
+    let success_msg = format!("Everything is fine");
+    println!("{}", style(success_msg).green());
+    Ok(())
 }
